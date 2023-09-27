d2ed23437289067e8400401a88da30d545644f10
William Bradshaw <w.a.bradshaw@gmail.com>
2023-09-27 22:34:46 +0100

    Algo that uses one strip and tries to repack columns (but only one way)

---
+++ web/src/app/arrangement/arrangement-algorithms/single-strip-refill-algorithm.ts
@@ -0,0 +1,154 @@
+import { Compartment } from "src/app/design/compartment";
+import { Arrangement } from "../arrangement";
+import { ArrangementAlgorithm } from "./arrangement-algorithm";
+import { BoxPlan } from "src/app/boxplan";
+import { PlannedCompartment } from "../planned-compartment";
+
+export class SingleStripRefillAlgorithm implements ArrangementAlgorithm {
+    plan(boxPlan: BoxPlan): Arrangement[] {
+        let compartments = boxPlan.getCompartments();
+
+        if(compartments.length > 1){
+            const woodWidth = boxPlan.getWood().size;
+            const targetArea = boxPlan.getTargetArea();
+
+            let sortedComponents = compartments.map(comp => this.toLengthFlippedComponent(comp))
+                                                 .sort(this.maxDimensionSorter);
+            
+            const first = sortedComponents.shift()!;
+            const firstCompartmentLength = first.maxSide;
+            const boxLength = firstCompartmentLength + (2 * woodWidth);
+ 
+            const plannedColumns: PlannedColumn[] = [];
+            plannedColumns.push({
+                width: first.minSide,
+                length: first.maxSide,
+                compartments: [first]
+            });
+            
+            let next = sortedComponents.shift();
+            while(next){                               
+                const columnCompartments: FlippableCompartment[] = [next];
+                const colWidth = next.minSide;
+                
+                let length = next.maxSide;
+                let remainingLength = firstCompartmentLength - length;
+                while(sortedComponents.length > 0 &&
+                      remainingLength > woodWidth + sortedComponents[sortedComponents.length-1].maxSide){
+
+                    const candidateIndex = sortedComponents.findIndex(comp => remainingLength >= comp.maxSide && comp.minSide <= colWidth);
+
+                    if(candidateIndex > -1){
+                        const candidate = sortedComponents.splice(candidateIndex, 1)[0];
+                        columnCompartments.push(candidate);
+                        const change = candidate.maxSide + woodWidth;
+                        length += change;
+                        remainingLength -= change;
+                    } else {
+                        remainingLength = 0;
+                    }
+                }
+
+                const nextCol: PlannedColumn = {
+                    width: colWidth,
+                    length: length,
+                    compartments: columnCompartments
+                };
+
+                plannedColumns.push(nextCol);
+                next = sortedComponents.shift();
+            }
+
+            console.log("Planned Columns");
+            console.log(plannedColumns);
+
+            let boxWidth = woodWidth;
+            let area = 0;
+            const plannedCompartments: PlannedCompartment[] = [];
+
+            plannedColumns.forEach(col => {
+                const difference = firstCompartmentLength - col.length;
+                const colCompartments = col.compartments;
+                const evenPadding: number = Math.floor(difference / colCompartments.length);
+                let unevenPadding: number = difference % colCompartments.length;
+
+                const x = boxWidth;
+                let y = woodWidth;
+                
+                colCompartments.forEach(compartment => {                    
+                    let compartmentLength = compartment.maxSide + evenPadding + unevenPadding;
+                    area += compartmentLength * col.width;
+
+                    plannedCompartments.push({
+                        id: compartment.id,
+                        name: compartment.name,
+                        depth: compartment.depth,
+                        x: x,
+                        y: y,
+                        width: col.width,
+                        length: compartmentLength,
+                        targetWidth: compartment.minSide,
+                        targetLength: compartment.maxSide,
+                        flipped: compartment.flipped
+                    });
+
+                    if (unevenPadding > 0) {
+                        unevenPadding--;
+                    }
+                    y += compartmentLength + woodWidth;
+                })
+                boxWidth += col.width + woodWidth;
+            });
+           
+            console.log("Planned Compartments");
+            console.log(plannedCompartments);
+
+            return [{
+                width: boxWidth,
+                length: boxLength,
+                compartments: plannedCompartments,
+                area: area,
+                wastedArea: area - targetArea
+            }];
+        } else {
+            return [];
+        }
+    }
+
+    toLengthFlippedComponent(comp: Compartment): FlippableCompartment {
+        return {
+            flipped: comp.width > comp.length,
+            maxSide: Math.max(comp.width, comp.length),
+            minSide: Math.min(comp.width, comp.length),
+            id: comp.id,
+            name: comp.name,
+            depth: comp.depth,
+            width: comp.width,
+            length: comp.length
+        }
+    }
+
+    maxDimensionSorter = (fCompA: FlippableCompartment, fCompB: FlippableCompartment) => {
+        if (fCompB.maxSide > fCompA.maxSide) {
+            return 1;
+        } else if (fCompB.maxSide < fCompA.maxSide) {
+            return -1;
+        } else {
+            return fCompB.minSide - fCompA.minSide;
+        }
+    }
+    
+}
+
+interface PlannedColumn {
+    width: number,
+    length: number,
+    compartments: FlippableCompartment[]
+}
+
+interface FlippableCompartment extends Compartment{
+    flipped: boolean,
+    maxSide: number,
+    minSide: number
+}
+
--- web/src/app/arrangement/arrangement.service.ts
+++ web/src/app/arrangement/arrangement.service.ts
@@ -4,6 +4,7 @@ import { Arrangement } from './arrangement';
 import { ArrangementAlgorithm } from './arrangement-algorithms/arrangement-algorithm';
 import { TestArrangementAlgorithm } from './arrangement-algorithms/test-arrangement-algorithm';
 import { NaiveStripAlgorithm } from './arrangement-algorithms/naive-strip-algorithm';
+import { SingleStripRefillAlgorithm } from './arrangement-algorithms/single-strip-refill-algorithm';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,24 @@ import { NaiveStripAlgorithm } from './arrangement-algorithms/naive-strip-algori
 export class ArrangementService {
 
   private _arrangementAlgorithms: ArrangementAlgorithm[] = [
-    new TestArrangementAlgorithm(),
-    new NaiveStripAlgorithm()
+    //new TestArrangementAlgorithm(),
+    new NaiveStripAlgorithm(),
+    new SingleStripRefillAlgorithm()
   ];
 
   planArrangements(boxPlan: BoxPlan): Arrangement[] {
-    let results =  this._arrangementAlgorithms.flatMap(algo => algo.plan(boxPlan));
+    let compartmentCount = boxPlan.getCompartments().length;
+    if (compartmentCount === 0){
+      return []
+    } else if (compartmentCount === 1) {
+      //TODO
+      return [];
+    } else{
+      let results =  this._arrangementAlgorithms.flatMap(algo => algo.plan(boxPlan));
 
-    results.sort((a,b) => a.wastedArea - b.wastedArea);
+      results.sort((a,b) => a.wastedArea - b.wastedArea);
 
-    return results;
+      return results;
+    }
   }
 }
