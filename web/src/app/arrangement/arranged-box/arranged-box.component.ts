926caed2aa31c8007f3a88488bde529868736149
William Bradshaw <w.a.bradshaw@gmail.com>
2023-10-04 14:58:08 +0100

    Box designs now display with a max height

--- web/src/app/arrangement/arranged-box/arranged-box.component.ts
+++ web/src/app/arrangement/arranged-box/arranged-box.component.ts
@@ -10,41 +10,43 @@ import { PlannedCompartment } from '../planned-compartment';
   template: `
     <div class="grid-box grid-2">
       <div class="card box-design selectable-card">
-        <svg [attr.viewBox]="'0 0 ' + arrangement.width + ' ' + arrangement.length"> 
-          <rect [attr.x]="0"
-                [attr.y]="0"
-                [attr.width]="arrangement.width"
-                [attr.height]="arrangement.length"
-                fill="black">
-          </rect>
-          <ng-container *ngFor="let compartment of arrangement.compartments">
-            <rect [attr.x]="compartment.x"
-                  [attr.y]="compartment.y"
-                  [attr.width]="compartment.width"
-                  [attr.height]="compartment.length"
-                  fill="red">
+        <div class="svg-wrapper">
+          <svg [attr.viewBox]="'0 0 ' + arrangement.width + ' ' + arrangement.length" preserveAspectRation="xMidYMid meet"> 
+            <rect [attr.x]="0"
+                  [attr.y]="0"
+                  [attr.width]="arrangement.width"
+                  [attr.height]="arrangement.length"
+                  fill="black">
             </rect>
-            <rect [attr.x]="compartment.x"
-                  [attr.y]="compartment.y"
-                  [attr.width]="compartment.targetWidth"
-                  [attr.height]="compartment.targetLength"
-                  fill="white">
-            </rect>
-            <text *ngIf="compartment.flipped === false"
-                  [attr.x]="compartment.x + compartment.width / 2"
-                  [attr.y]="compartment.y + compartment.length / 2"
-                  text-anchor="middle"
-                  dominant-baseline="middle"
-                  [attr.font-size]="getFontSize(compartment)">{{ compartment.name }}</text>
-            <text *ngIf="compartment.flipped"
-                  [attr.x]="compartment.x + compartment.width / 2"
-                  [attr.y]="compartment.y + compartment.length / 2"
-                  text-anchor="middle"
-                  dominant-baseline="middle"
-                  [attr.font-size]="getFontSize(compartment)"
-                  [attr.transform]="'rotate(90,' + (compartment.x + compartment.width / 2) + ',' + (compartment.y + compartment.length / 2) + ')'">{{ compartment.name }}</text>
-          </ng-container>
-        </svg>
+            <ng-container *ngFor="let compartment of arrangement.compartments">
+              <rect [attr.x]="compartment.x"
+                    [attr.y]="compartment.y"
+                    [attr.width]="compartment.width"
+                    [attr.height]="compartment.length"
+                    fill="red">
+              </rect>
+              <rect [attr.x]="compartment.x"
+                    [attr.y]="compartment.y"
+                    [attr.width]="compartment.targetWidth"
+                    [attr.height]="compartment.targetLength"
+                    fill="white">
+              </rect>
+              <text *ngIf="compartment.flipped === false"
+                    [attr.x]="compartment.x + compartment.width / 2"
+                    [attr.y]="compartment.y + compartment.length / 2"
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                    [attr.font-size]="getFontSize(compartment)">{{ compartment.name }}</text>
+              <text *ngIf="compartment.flipped"
+                    [attr.x]="compartment.x + compartment.width / 2"
+                    [attr.y]="compartment.y + compartment.length / 2"
+                    text-anchor="middle"
+                    dominant-baseline="middle"
+                    [attr.font-size]="getFontSize(compartment)"
+                    [attr.transform]="'rotate(90,' + (compartment.x + compartment.width / 2) + ',' + (compartment.y + compartment.length / 2) + ')'">{{ compartment.name }}</text>
+            </ng-container>
+          </svg>
+        </div>
         <div class="card-contents">
           <div class="card-title">{{arrangement.width}} mm x {{arrangement.length}} mm : {{getEfficiency()}}% Efficient</div>
           <div class="card-description">
